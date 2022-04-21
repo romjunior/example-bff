2adfd539e4c7258c7492f9f705f3c26195c16253
Romualdo Junior <romualdo.jrr@gmail.com>
2022-04-21 19:58:50 -0300

    first commit

---
+++ src/main/java/com/estudo/reactivebff/ReactiveBffApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.reactivebff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveBffApplication.class, args);
+	}
+
+}
